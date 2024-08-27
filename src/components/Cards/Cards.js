e35956f88d2d0fdb1c047e99b3bf24d582dc00f6
Rodrigo Honorio <rodrigohono43@outlook.com>
2024-08-26 22:32:02 -0300

    atualização

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,6 +3,9 @@ import styles from './Cards.module.css'
 import fio from '../..//assets/fio a fio.jpg'
 import sobrancelha from '../../assets/sobrancelha.jpg'
 import rena from '../../assets/rena.jpg'
+import boca from '../../assets/boca.jpg'
+import contornoBoca from '../../assets/contorno do labio.jpg'
+import bocaHomem from '../../assets/bocahomem.jpg'
 const Cards = () => {
     return (
         <>
@@ -22,7 +25,7 @@ const Cards = () => {
                         Ideal para quem deseja ter as sobrancelhas preechidas.</p>
                 </div>
                 <div className={styles.card}>
-                    <img src="" alt="" />
+                    <img src={sobrancelha} alt="" />
                     <h4>Micropigmentação MIX</h4>
                     <p>É realizada é técnica esfumada em conjunto com a técnica fio a fio
                         Durabilidade média entre 8 e 18 meses.</p>
@@ -40,24 +43,19 @@ const Cards = () => {
             </div>
             <div className={styles.card_container}>
                 <div className={styles.card}>
-                    <img src="" alt="" />
+                    <img src={contornoBoca} alt="" />
                     <h4>Micropigmentação labial Revitalização</h4>
-                    <p>É realizada é técnica esfumada em conjunto com a técnica fio a fio
-                        Durabilidade média entre 8 e 18 meses.</p>
+                    <p>Transforme seus lábios com micropigmentação labial, destacando o contorno e a cor natural, e revitalização, que promove hidratação profunda e rejuvenescimento.</p>
                 </div>
                 <div className={styles.card}>
-                    <img src={rena} alt="micro de rena" />
+                    <img src={bocaHomem} alt="micro de rena" />
                     <h4>Micropigmentação labial Neutralização</h4>
-                    <p>Aparência similar ao prenchimento com lápis,
-                        Durabilidade média de 3 meses.
-                        Ideal para pessoas com receio de realizar uma técnica de longa duração, </p>
+                    <p>Corrija e uniformize o tom dos seus lábios! A neutralização é ideal para quem tem lábios escurecidos ou com manchas, proporcionando uma cor natural e harmoniosa. Realce sua beleza com lábios mais claros e definidos. Agende sua sessão!</p>
                 </div>
                 <div className={styles.card}>
-                    <img src={rena} alt="micro de rena" />
+                    <img src={boca} alt="micro de rena" />
                     <h4>Micropigmentação labial Efeito Batom</h4>
-                    <p>Aparência similar ao prenchimento com lápis,
-                        Durabilidade média de 3 meses.
-                        Ideal para pessoas com receio de realizar uma técnica de longa duração, </p>
+                    <p>ATenha lábios perfeitos o tempo todo! A técnica de micropigmentação labial com efeito batom deixa seus lábios sempre com cor vibrante e contorno definido, como se estivesse usando batom. Realce sua beleza com praticidade e durabilidade. Agende sua sessão! </p>
                 </div>
             </div>
         </>
