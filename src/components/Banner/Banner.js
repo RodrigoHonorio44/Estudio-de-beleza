d45efd3c156807d5f99533360ef6d65a18aed329
Rodrigo Honorio <rodrigohono43@outlook.com>
2024-08-29 10:39:07 -0300

    atualizando o footer

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,14 +2,15 @@ import React from 'react'
 import banner from '../../assets/sobrancelhas.jpg'
 import styles from './Banner.module.css'
 import icon from '../../assets/icons8-whatsapp-80.png'
-
+import { IoLogoWhatsapp } from "react-icons/io";
 const Banner = () => {
     return (
         <>
             <div className={styles.titulo}><h1>Micropigmentação Sobrancelhas</h1></div>
             <div className={styles.banner}>
                 <div className={styles.agendar}>
-                    <a href='https://wa.me/5521975966330' target="_blank"><img src={icon} alt="whatsapp" /></a><p > Clique Aqui</p>
+                    <a href='https://wa.me/5521975966330' target="_blank"><IoLogoWhatsapp size={30} style={{ color: '#22a241' }} /></a>
+                    <p > Clique Aqui</p>
                     <p>Se desejar agendar uma avaliação</p>
                 </div>
                 <div className={styles.avaliação}>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa6'
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaTiktok } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 const Footer = () => {
     return (
-        <div className={styles.footer}>
-            <p>© 2024 Meu Nome. Todos os direitos reservados.</p>
+        <>
+            <div className={styles.footer}>
 
-        </div>
+                <div className={styles.redes}>
+                    <div className={styles.funcionamento}>
+                        <h4>HORÁRIO DE FUNCIONAMETO</h4>
+
+                        <p>Segunda à Sexta 9h às 18:00</p>
+                        <p>Sábado das 9h ás 17h</p>
+                    </div>
+                    <div className={styles.socias}>
+                        <h3>Redes Socias</h3>
+                        <a href="https://www.instagram.com/susigreycepmu/" target="_blank" rel="noopener noreferrer"><FaInstagram size={30} style={{ color: ' #ffffff' }} /></a>
+                        <a href="https://www.tiktok.com/@susigreycepmu" target="_blank" rel="noopener noreferrer"><FaTiktok size={30} style={{ color: ' #ffffff' }} /></a>
+                        <a href="https://www.facebook.com/susigreyce.sobrancelhas" target="_blank" rel="noopener noreferrer"><FaFacebook size={30} style={{ color: ' #ffffff' }} /></a>
+                    </div>
+                </div>
+
+            </div>
+            <div className={styles.rodape}>
+                <p>© 2024 Honório. Todos os direitos reservados.</p>
+            </div>
+
+
+        </>
     )
 }
 
